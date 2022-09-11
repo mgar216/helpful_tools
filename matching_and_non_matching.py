36a480c06c429dadedc6efb56399de8ab5dea26b
mgar216 <28987804+mgar216@users.noreply.github.com>
2022-09-10 22:16:11 -0600

    Update matching_and_non_matching.py
    Added example functions -- requires fuzzy_pandas and pandas.

--- matching_and_non_matching.py
+++ matching_and_non_matching.py
@@ -75,3 +75,61 @@ idx = match_idx[match_idx.applymap(lambda x: len(x) == 0)].dropna().index.to_lis
 non_match_idx = matches.iloc[idx].drop(col1, axis=1)['index'].to_list()
 not_matched = df2.iloc[non_match_idx]
 print('Non-Matched:\n', not_matched)
+
+
+# Example functions shown below that implement the above:
+
+import pandas as pd
+import fuzzy_pandas as fpd
+
+def matchToDataFrame(
+                        left_df: pd.DataFrame,
+                        right_df: pd.DataFrame,
+                        on: list,
+                        match_to_left: bool=True,
+                        ignore_case: bool=True,
+                        method: str='levenshtein',
+                        threshold: float=1.0,
+                        ) -> pd.DataFrame:
+  if not match_to_left:
+    left_df, right_df = right_df, left_df
+  matches = fpd.fuzzy_merge(right_df.reset_index(), left_df,
+                          on=on,
+                          ignore_case=ignore_case,
+                          method=method,
+                          threshold=threshold,
+                          keep='all',
+                          join='inner'
+                          ).set_index('index')
+  idx = matches.index.to_list()
+  matched = right_df.loc[idx]
+  return matched
+
+def differingDataFrame(
+                            left_df: pd.DataFrame,
+                            right_df: pd.DataFrame,
+                            on: list,
+                            match_to_left: bool=True,
+                            ignore_case: bool=True,
+                            method: str='levenshtein',
+                            threshold: float=1.0,
+                            ) -> pd.DataFrame:
+  if not match_to_left:
+    left_df, right_df = right_df, left_df
+  matches = fpd.fuzzy_merge(left_df, right_df.reset_index(),
+                          on=on,
+                          ignore_case=ignore_case,
+                          method=method,
+                          threshold=threshold,
+                          keep='all',
+                          join='right-outer'
+                          )
+  matches.columns = ['_left_' + i if n < len(left_df.columns) else i for n, i in enumerate(matches)]
+  col1 = matches.filter(regex='_left_').columns.to_list()
+  match_idx = matches.loc[:, col1]
+  idx = match_idx[match_idx.applymap(lambda x: len(x) == 0)].dropna().index.to_list()
+  non_match_idx = matches.iloc[idx].drop(col1, axis=1)['index'].to_list()
+  non_matched = right_df.iloc[non_match_idx]
+  return non_matched
+
+
