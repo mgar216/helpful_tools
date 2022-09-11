4415c250ab54603e55ba73c939165f347d56bedd
mgar216 <28987804+mgar216@users.noreply.github.com>
2022-09-10 22:02:50 -0600

    Update helptools.py
    Added matchToRightDataFrame and differingToRightDataFrame.

--- helptools.py
+++ helptools.py
@@ -1,6 +1,8 @@
 import os, pandas as pd
 from pathlib import Path
 from stat import S_IREAD, S_IRGRP, S_IROTH, S_IWRITE, S_IWGRP, S_IWOTH
+import pandas as pd
+import fuzzy_pandas as fpd
 
 def getAgedDirectoryFiles(base_directory: str, age: int=0):
   # Defaults to most recent last modified, while age=1 is the one last modified prior to that.
@@ -48,4 +50,49 @@ def writeToReadOnlyXLSX(df: pd.DataFrame, file_location: str):
   fileToReadOnlyMode(file_location)
   return 1
 
- 
+
+def matchToRightDataFrame(
+                        left_df: pd.DataFrame,
+                        right_df: pd.DataFrame,
+                        on: list,
+                        ignore_case: bool=True,
+                        method: str='levenshtein',
+                        threshold: float=1.0,
+                        ):
+  matches = fpd.fuzzy_merge(right_df.reset_index(), left_df,
+                          on=on,
+                          ignore_case=ignore_case,
+                          method=method,
+                          threshold=threshold,
+                          keep='all',
+                          join='inner'
+                          ).set_index('index')
+  idx = matches.index.to_list()
+  matched = right_df.loc[idx]
+  return matched
+
+
+def differingToRightDataFrame(
+                            left_df: pd.DataFrame,
+                            right_df: pd.DataFrame,
+                            on: list,
+                            ignore_case: bool=True,
+                            method: str='levenshtein',
+                            threshold: float=1.0,
+                            ):
+  matches = fpd.fuzzy_merge(left_df, right_df.reset_index(),
+                          on=on,
+                          ignore_case=ignore_case,
+                          method=method,
+                          threshold=threshold,
+                          keep='all',
+                          join='right-outer'
+                          )
+  matches.columns = ['_left_' + i if n < len(left_df.columns) else i for n, i in enumerate(matches)]
+  col1 = matches.filter(regex='_left_').columns.to_list()
+  match_idx = matches.loc[:, col1]
+  idx = match_idx[match_idx.applymap(lambda x: len(x) == 0)].dropna().index.to_list()
+  non_match_idx = matches.iloc[idx].drop(col1, axis=1)['index'].to_list()
+  non_matched = right_df.iloc[non_match_idx]
+  return non_matched
+
